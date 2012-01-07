ee9f3ca153ef8ba88d5e677761782a1c700b10a8
tomasperezv <tom@0x101.com>
2012-01-07 20:10:57 +0100

    #1 renaming javascript files.

--- server.js
+++ server.js
@@ -1,46 +1,55 @@
 var sys = require("sys"),
-		http = require("http"),
-		url = require("url"),
-		path = require("path"),
-		fs = require("fs"),
-		ServerHelper = require("./js/server/serverHelper");
+	http = require("http"),
+	url = require("url"),
+	path = require("path"),
+	fs = require("fs"),
+	ServerHelper = require("./js/server/server-helper.js");
 
 http.createServer(function(request, response) {
-		var uri = url.parse(request.url).pathname;
-		var filename = path.join(process.cwd(), ServerHelper.constants.DEFAULT_DOCUMENT);
-		sys.puts(filename);
-		path.exists(filename, function(exists) {
-				if(!exists) {
-						ServerHelper.writeError(response, ServerHelper.constants.NOT_FOUND);
-						response.end();
-						return;
+
+	var uri = url.parse(request.url).pathname;
+	var filename = path.join(process.cwd(), ServerHelper.constants.DEFAULT_DOCUMENT);
+	sys.puts(filename);
+
+	path.exists(filename, function(exists) {
+
+		if(!exists) {
+			ServerHelper.writeError(response, ServerHelper.constants.NOT_FOUND);
+			response.end();
+			return;
+		}
+		
+		fs.readFile(filename, "binary", function(err, file) {
+	
+			if(err) {
+	
+				ServerHelper.writeError(response, ServerHelper.constants.SERVER_ERROR, err);
+	
+			} else if (ServerHelper.canServe(filename)) {
+	
+				try {
+
+					sys.puts(filename);
+					ServerHelper.writeHeader(response, filename);
+
+					response.write(file, "binary");
+
+				} catch (Error) {
+
+					sys.puts(filename);
+					ServerHelper.writeHeader(response, ServerHelper.constants.DEFAULT_DOCUMENT);
+
+					response.write(file, "binary");
+
 				}
 	
-				fs.readFile(filename, "binary", function(err, file) {
-						if(err && !ServerHelper.canSolve(err) ) {
-								ServerHelper.writeError(response, ServerHelper.constants.SERVER_ERROR, err);
-						} else if (false && ServerHelper.canSolve(err)) {
-								var newFilename = ServerHelper.solve(filename);
-								sys.puts(newFilename);
-								fs.readFile(newFilename, "binary", function(err, file) {
-										ServerHelper.writeHeader(response, newFilename);
-										response.write(file, "binary");
-								});
-						} else if (ServerHelper.canServe(filename)) {
-								try {
-										sys.puts(filename);
-										ServerHelper.writeHeader(response, filename);
-										response.write(file, "binary");
-								} catch (Error) {
-										sys.puts(filename);
-										ServerHelper.writeHeader(response, 'index.htm');
-										response.write(file, "binary");
-								}
-						}
-
-						response.end();
-				});
+			}
+	
+			response.end();
+	
 		});
+	});
+
 }).listen(ServerHelper.constants.PORT);
 
 sys.puts("Server running at " + ServerHelper.constants.PORT + " port.");
