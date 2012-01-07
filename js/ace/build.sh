5bb9c48364baed4aa892b60687aeb3eed3ddc8ec
tomasperezv <tom@0x101.com>
2012-01-07 20:12:43 +0100

    #2 deleting unused code.

--- js/ace/build.sh
+++
@@ -1,7 +0,0 @@
-rm aceBuild.js
-cat ace.js >> aceBuild.js
-echo "\n" >> aceBuild.js
-cat theme-twilight.js >> aceBuild.js
-echo "n" >> aceBuild.js
-cat mode-javascript.js >> aceBuild.js
-echo "\n" >> aceBuild.js
